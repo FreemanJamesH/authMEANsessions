e874e2f6469d544080f47865ea1f8c35dabf8d8c
James Freeman <freeman.james.h@gmail.com>
2016-09-17 17:34:11 -0600

    set up routes

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 let express = require('express')
 let app = express();
+app.locals.pretty = true;
 app.set('view engine', 'jade');
+
 
 app.get('/', function(req, res){
   res.render('index')
+})
+
+app.get('/register', function(req, res){
+  res.render('register')
+})
+
+app.get('/login', function (req, res) {
+  res.render('login')
+})
+
+app.get('/dashboard', function (req, res) {
+  res.render('dashboard')
+})
+
+app.get('/logout', function (req, res) {
+  res.redirect('/')
 })
 
 app.listen(3000)
